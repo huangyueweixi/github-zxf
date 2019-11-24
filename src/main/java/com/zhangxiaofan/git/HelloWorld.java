7f08211068ed5e121bf895f5ddd65953e3674baa
zhangxiaofan <21219929@163.com>
2019-11-24 16:10:50 +0800

    version 1.0 第一次提交

---
+++ src/main/java/com/zhangxiaofan/git/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zhangxiaofan.git;
+
+/**
+ * @author ZhangXiaoFan
+ * @create 2019-11-24 15:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
